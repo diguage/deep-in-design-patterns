a16020e41fc9fd08e308bf7f860f6eec657d1b53
diguage <leejun119@gmail.com>
2017-05-16 11:20:05 +0800

    增加包以及Client类

---
+++ src/main/java/com/diguage/didp/mediator/Client.java
@@ -0,0 +1,12 @@
+package com.diguage.didp.mediator;
+
+/**
+ * 代理类
+ *
+ * @author D瓜哥，http://www.diguage.com/
+ * @since 2017-05-16
+ */
+public class Client {
+  public static void main(String[] args) {
+  }
+}
