3562fcd266b38d3eed2c753669a02f0a90a811c7
diguage <leejun119@gmail.com>
2017-05-21 00:36:59 +0800

    增强将类引用添加到文档中的脚步：递归调用自目录，增加去重复添加的判断

--- c2a.sh
+++ c2a.sh
@@ -6,6 +6,41 @@ then
   sed=`which gsed`
 fi
 
+# 第一个参数：adoc 文件路径
+# 第二个参数：需要扫描的文件或者目录
+# 第二个参数：基础包路径
+function recursiveAddSrcFileToAdoc {
+  local afn=$1;
+  local pp=$2;
+  local bp=$3;
+  if [[ -f $pp ]]; then
+
+    # 判断所需要添加的类是否已经包含在文件中，没有才执行添加操作
+    if `grep -q ${bp} ${afn}; [ $? -eq 1 ]`; then
+# 是文件就加入到文件中
+include_src=$(cat<<EOF
+
+[source,{java_source_attr}]
+.${bp} 类
+----
+include::{source_dir}/${bp}[]
+----
+
+EOF
+)
+        echo "${include_src}" >> ${afn}
+      fi
+  elif [[ -d $pp ]]; then
+    # 如果是目录，则递归扫描该目录
+    for f in `ls $pp `;
+    do
+      recursiveAddSrcFileToAdoc $afn "${pp}/${f}" "${bp}/${f}"
+    done
+  fi
+}
+
+
+
 project_dir=`pwd`
 java_base_pkg="com.diguage.didp"
 base_pkg_dir="${project_dir}/src/main/java/`echo $java_base_pkg | $sed 's/\./\//g'`"
@@ -13,34 +48,14 @@ book_dir="${project_dir}/book"
 
 for uml in `ls ${project_dir}/puml/*.puml`;
 do
-  echo $uml;
-
   file_name=`echo $uml | awk -F/ '{print $NF}' | awk -F. '{print $1}'`
   pkg=`echo $file_name | $sed 's/-//g'`
   pkg_path=${base_pkg_dir}/${pkg}
 
-  echo "${book_dir}/${file_name}.adoc"
+  adoc_file_name="${book_dir}/${file_name}.adoc"
 
-  # 遍历数组，获取下标以及各个元素
-  for cf in `ls $pkg_path`;
-  do
-
-    # 提取类名
-    class_path="/${pkg}/${cf}";
-
-    class_name=`echo $cf | awk -F. '{print $1}'`
-
-include_src=$(cat<<EOF
-
-[source,{java_source_attr}]
-.${class_name} 类
-----
-include::{source_dir}${class_path}[]
-----
-
-EOF
-)
-
-    echo "${include_src}"
-  done
+  recursiveAddSrcFileToAdoc $adoc_file_name $pkg_path $pkg
 done
+
+
+echo "Finish dealing!"
