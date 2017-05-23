abd9624eda6ef664788f9517526bd1522877071e
diguage <leejun119@gmail.com>
2017-05-23 10:39:55 +0800

    修正脚本中不能正确处理空类的继承关系的问题

--- u2c.sh
+++ u2c.sh
@@ -62,43 +62,40 @@ EOF
     echo "public ${class_sign} {" >> $class_file_name
 
     # 如果类为空，则不需要继续处理
-    if [ $[$start+1] -eq $end ]
+    if [ $[$start+1] -ne $end ]
     then
-      echo "}" >> $class_file_name
-      continue;
+      # 截取出方法来
+      # 将 + 换成 public
+      # 将 - 换成 private
+      # 去掉 abstract 两边的大括号
+      # 去掉 static 两边的大括号
+      # 调整方法参数声明的语法 -- 这里只有一个参数
+      # 调整方法返回值的语法
+      # 添加没有声明返回值的方法的语法
+      # 调整抽象方法添加分号结尾
+      # 调整非抽象方法添加大括号
+      # 调整属性声明的语法
+      $sed -n "$[${start}+1], $[${end}-1]p" $uml \
+        | $sed "s/+/public/g" \
+        | $sed "s/-/private/g" \
+        | $sed "s/{\(abstract\)}/\1/g" \
+        | $sed "s/{\(static\)}/\1/g" \
+        | $sed "s/(\(\w*\)\s*:\s*\(\w*\))/(\2 \1)/g" \
+        | $sed "s/\s*\(\w*(.*)\)\s*:\s*\(\w*\)/ \2 \1/g" \
+        | $sed "s/\(public\|public\s\+abstract\)\s\+\(\w*(\)/\1 void \2/g" \
+        | $sed "s/\(abstract.*)\)\s*$/\1;/g" \
+        | $sed "s/)[^;]*$/) {\n  }/g" \
+        | $sed "s/\(private\)\s*\(\w*\)\s*:\s*\(\w*\)/\1 \3 \2;/g" \
+        >> $class_file_name;
     fi
-
-
-    # 截取出方法来
-    # 将 + 换成 public
-    # 将 - 换成 private
-    # 去掉 abstract 两边的大括号
-    # 去掉 static 两边的大括号
-    # 调整方法参数声明的语法 -- 这里只有一个参数
-    # 调整方法返回值的语法
-    # 添加没有声明返回值的方法的语法
-    # 调整抽象方法添加分号结尾
-    # 调整非抽象方法添加大括号
-    # 调整属性声明的语法
-    $sed -n "$[${start}+1], $[${end}-1]p" $uml \
-      | $sed "s/+/public/g" \
-      | $sed "s/-/private/g" \
-      | $sed "s/{\(abstract\)}/\1/g" \
-      | $sed "s/{\(static\)}/\1/g" \
-      | $sed "s/(\(\w*\)\s*:\s*\(\w*\))/(\2 \1)/g" \
-      | $sed "s/\s*\(\w*(.*)\)\s*:\s*\(\w*\)/ \2 \1/g" \
-      | $sed "s/\(public\|public\s*abstract\)\s*\(\w+(\)/\1 void \2/g" \
-      | $sed "s/\(abstract.*)\)\s*$/\1;/g" \
-      | $sed "s/)[^;]*$/) {\n  }/g" \
-      | $sed "s/\(private\)\s*\(\w*\)\s*:\s*\(\w*\)/\1 \3 \2;/g" \
-      >> $class_file_name;
 
     # 结束类
     echo "}" >> $class_file_name;
 
     # 处理泛化和实现
-    relationship=`grep -E "<\|[-.]{2}\s*${class_name}" $uml | $sed "s/\(\w\+\) <|\(..\) \(.*\)/\2/g"`
+    relationship=`grep -E "<\|[-.]{2}\s*${class_name}" $uml | $sed "s/\(\w\+\)\s*<|\(..\) \(.*\)/\2/g"`
     super_class=`grep -E "<\|[-.]{2}\s*${class_name}" $uml | awk '{print $1}'`
+    # echo "=${class_name}=__=${relationship}=__=${super_class}="
     if [[ -n $super_class ]]; then
       if [ "$relationship" = ".." ]; then
         relationship=implements
@@ -108,7 +105,8 @@ EOF
         relationship=extends
       fi
 
-      $sed -i "s/$class_name {/$class_name ${relationship} ${super_class} {/g" $class_file_name
+      # echo "=${class_name}=__=${relationship}=__=${super_class}="
+      $sed -i "s/$class_name\s\+{/$class_name ${relationship} ${super_class} {/g" $class_file_name
     fi
   done
 done
