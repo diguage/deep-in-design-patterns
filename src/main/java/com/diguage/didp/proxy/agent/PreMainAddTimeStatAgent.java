d7804b4da5505641c371d3b5dae3e780ea70e740
diguage <leejun119@gmail.com>
2022-07-31 00:37:37 +0800

    解决编译及图片等问题

--- src/main/java/com/diguage/didp/proxy/agent/PreMainAddTimeStatAgent.java
+++ src/main/java/com/diguage/didp/proxy/agent/PreMainAddTimeStatAgent.java
@@ -4,7 +4,10 @@ import com.diguage.didp.proxy.asm.ProfilerClassAdapter;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
+import java.security.ProtectionDomain;
 
 /**
  * @author D瓜哥, https://www.diguage.com/
@@ -14,20 +17,24 @@ public class PreMainAddTimeStatAgent {
   public static void premain(String agentArgs, Instrumentation instrumentation) {
     System.out.println("agentArgs: " + agentArgs);
     instrumentation.addTransformer(
-        (loader, className, classBeingRedefined, protectionDomain, classfileBuffer) -> {
-          if (className.equals("com/diguage/didp/proxy/asm/Account")) {
-            System.out.println("meet com.diguage.didp.proxy.asm.Account");
-            ClassReader cr = new ClassReader(classfileBuffer);
-            ClassWriter cw =
-                new ClassWriter(
-                    (ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES));
-            ProfilerClassAdapter classAdapter = new ProfilerClassAdapter(cw);
-            cr.accept(classAdapter, ClassReader.SKIP_DEBUG);
-            return cw.toByteArray();
-          } else {
-            System.out.println("\nload: " + className);
-            return classfileBuffer;
+        new ClassFileTransformer() {
+          @Override
+          public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+            if (className.equals("com/diguage/didp/proxy/asm/Account")) {
+              System.out.println("meet com.diguage.didp.proxy.asm.Account");
+              ClassReader cr = new ClassReader(classfileBuffer);
+              ClassWriter cw =
+                  new ClassWriter(
+                      (ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES));
+              ProfilerClassAdapter classAdapter = new ProfilerClassAdapter(cw);
+              cr.accept(classAdapter, ClassReader.SKIP_DEBUG);
+              return cw.toByteArray();
+            } else {
+              System.out.println("\nload: " + className);
+              return classfileBuffer;
+            }
           }
-        });
+        }
+    );
   }
 }
